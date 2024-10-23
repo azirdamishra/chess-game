497f85a9aef004c9ad3e33c5d1b3c25722235b6d
Adriza Mishra <driz@Adrizas-MacBook-Pro.local>
2024-10-23 18:16:12 +0530

    base frontend skeleton

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import { Landing } from './screens/Landing';
+import {Game} from './screens/Game';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} /> {/* 👈 Renders at /app/ */}
+        <Route path="/game" element={<Game />} /> 
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App
